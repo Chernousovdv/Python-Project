e6a817ff323bb4a1d64d7811180f2104a42a44f5
Chernousov Danila <113239558+Chernousovdv@users.noreply.github.com>
2022-11-21 23:43:42 +0300

    Update Typewriter.py

--- Typewriter.py
+++ Typewriter.py
@@ -40,23 +40,23 @@ class Window:
         while self.is_active:
             time.sleep(0.1)
             self.time_passed += 0.1
-
-            self.TimePassedStat.config(text=f"{self.time_passed:.0f} s")
+            self.TimePassedStat.config(text=f"{self.time_passed:.1f} s")
             CPM = 60 * (self.char_count / self.time_passed)
-            self.CPMstat.config(text=f"CPM: {CPM:.0f}")
+            self.CPMstat.config(text=f"CPM: {CPM:.2f}")
 
     def is_valid(self, newval):
+
         if (len(newval) == len(self.current_text) and newval[-1] == self.current_text[self.current_character_index]):
             self.current_character_index = 0
-            self.UserInputSace.delete(0, len(self.current_text))
+            self.UserInputSpace.delete(0, len(self.current_text))
             self.NewText()
             self.word_counter += 1
             if not self.time_passed == 0:
                 self.words_per_minute = 60 * (self.word_counter / self.time_passed)
             else:
                 self.words_per_minute = 0
-            self.UserInputSace.after_idle(lambda: self.UserInputSace.configure(validate="all"))
-            self.WPMstat.config(text=f"{self.words_per_minute:.0f} WPM")
+            self.UserInputSpace.after_idle(lambda: self.UserInputSpace.configure(validate="all"))
+            self.WPMstat.config(text=f"{self.words_per_minute:.2f} WPM")
             return True
         if (len(newval) != 0 and newval[-1] == self.current_text[self.current_character_index]):
             if newval[-1] == " ":
@@ -65,11 +65,12 @@ class Window:
                     self.words_per_minute = 60 * (self.word_counter / self.time_passed)
                 else:
                     self.words_per_minute = 0
-                self.WPMstat.config(text=f"{self.words_per_minute:.0f} WPM")
+                self.WPMstat.config(text=f"{self.words_per_minute:.2f} WPM")
             self.current_character_index += 1
             self.char_count += 1
             return True
         else:
+
             self.error_count += 1
             pickle.dump(self.error_count, open("error_log.dat", "wb"))
             self.ErrorsStat.config(text=f"{self.error_count} errors")
@@ -78,23 +79,25 @@ class Window:
     def OpenHelp(self):
         Help()
 
-    def ClearText(self):
-        self.UserInputSace.delete(0, len(self.UserInputSace.get()))
-
     def Reset(self):
 
-        self.is_active = False
         self.error_count = 0
         pickle.dump(self.error_count, open("error_log.dat", "wb"))
         self.ErrorsStat.config(text=f"{self.error_count} errors")
-        self.time_passed = 0.1
-        self.current_character_index = len(self.current_text) - 1
-        self.UserInputSace.destroy()
-        self.UserInputSace = tk.Entry(self.root, width=80, bg='white', validate="key", validatecommand=self.check)
-        self.UserInputSace.place(anchor="n", x=300, y=150)
+
+        self.current_character_index = 0
+
+        self.StartProgramm(True)
+        self.time_passed = 0
+        self.UserInputSpace.destroy()
+        self.check = (self.root.register(self.is_valid), "%P")
+        self.UserInputSpace = tk.Entry(self.root, width=80, bg='white', validate="key", validatecommand=self.check)
+        self.UserInputSpace.place(anchor="n", x=300, y=150)
         self.current_character_index = 0
         self.word_counter = 0
-        self.WPMstat.config(text="0 WPM")
+        self.words_per_minute = 0.00
+        self.WPMstat.config(text=f"{self.words_per_minute:.2f} WPM")
+
         self.NewText()
         self.char_count = 0
 
@@ -103,6 +106,7 @@ class Window:
         self.time_passed = 0
         self.current_character_index = 0
         self.word_counter = 0
+        self.error_count = 0
         if not exists("error_log.dat"):
             pickle.dump(0, open("error_log.dat"), "wb")
         self.char_count = 0
@@ -121,9 +125,9 @@ class Window:
         self.TextLabel.place(anchor="n", x=300, y=100)
         self.check = (self.root.register(self.is_valid), "%P")
         self.g = tk.Entry
-        self.UserInputSace = tk.Entry(self.root, width=80, bg='white', validate="key", validatecommand=self.check)
-        self.UserInputSace.bind("<KeyPress>", self.StartProgramm)
-        self.UserInputSace.place(anchor="n", x=300, y=150)
+        self.UserInputSpace = tk.Entry(self.root, width=80, bg='white', validate="key", validatecommand=self.check)
+        self.UserInputSpace.bind("<KeyPress>", self.StartProgramm)
+        self.UserInputSpace.place(anchor="n", x=300, y=150)
         self.CPMstat = tk.Label(self.root, text=("CPM:", 0), bg='#050A1A', fg="white", font=("Arial", 10))
         self.CPMstat.place(anchor="n", x=300, y=400)
         self.TimePassedStat = tk.Label(self.root, text=(0, "s"), bg='#050A1A', fg="white", font=("Arial", 10))
@@ -131,7 +135,7 @@ class Window:
         self.ErrorsStat = tk.Label(self.root, text=f"{self.error_count} errors", bg='#050A1A', fg="white", font=("Arial", 10))
         self.ErrorsStat.place(anchor="n", x=300, y=250)
         self.words_per_minute = 0
-        self.WPMstat = tk.Label(self.root, text=f"{self.words_per_minute:.0f} WPM", bg='#050A1A', fg="white", font=("Arial", 10))
+        self.WPMstat = tk.Label(self.root, text=f"{self.words_per_minute:.2f} WPM", bg='#050A1A', fg="white", font=("Arial", 10))
         self.WPMstat.place(anchor="n", x=300, y=275)
         self.ResetErrorButton = tk.Button(self.root, text="Reset", bg='#050A1A', fg="white", font=("Arial", 15), command=self.Reset)
         self.ResetErrorButton.place(anchor="n", x=300, y=180)
@@ -142,4 +146,3 @@ class Window:
 
 
 Window()
-
